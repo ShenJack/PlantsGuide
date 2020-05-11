ca27a8898d29de7e632abc1ba36db54d28a20a76
ShenJack <357875929@qq.com>
2020-05-11 23:15:11 +0800

    增加VR入口

--- src/views/plantCard/index.tsx
+++ src/views/plantCard/index.tsx
@@ -5,23 +5,39 @@ import {useStore} from "../../store";
 import {STORES} from "../../store/const";
 import {PlantDescription} from "./plantDescription";
 import {IconButton} from "../../component/iconButton";
+import {getDispatch} from "../../store/dispatches";
+import {IconTag} from "../../component/iconTag";
+import {PlantType} from "../../utils/const";
 
 
 interface Props {
   hideBackButton?: boolean
+  instance?: any
 }
 
 function gotoList() {
   appHistory.push('/list')
 }
 
-export function PlantCard(props: Props) {
+function gotoMap(instance) {
+  appHistory.push('/map')
+  getDispatch(STORES.PLANT_STORE)({
+    currentPlantInstance: instance,
+  })
+}
 
+export function PlantCard(props: Props) {
   let [plantStore] = useStore(STORES.PLANT_STORE)
-  let [plants, dispatchPlants] = useState([]);
-  const plant = plantStore.currentPlantDetail;
+  let plant;
+  if (!props.instance) {
+    plant = plantStore.currentPlantDetail;
+  } else {
+    plant = props.instance.plant;
+  }
+
+  //刷新页面之后没有plant了，自动跳转到列表
   useEffect(() => {
-    if (plant == null) {
+    if (plant == null && !props.instance) {
       gotoList()
     }
   }, [])
@@ -36,26 +52,61 @@ export function PlantCard(props: Props) {
       </IconButton>}
       {plant &&
       <div>
-        <img className="plant-card-img" src={plant && plant.coverUrl} alt={plant.name}/>
+        <div className={'plant-card-header'}>
+          <img className="plant-img" src={plant && plant.coverUrl} alt={plant.name}/>
+          <div className="enter-vr">
+            <div className="button" onClick={() => appHistory.push('/vr')}>
+              VR
+            </div>
+          </div>
+        </div>
         <div className="plant-details">
           <h5 className="plant-name">
             {plant.name}
           </h5>
+          <div className="tags">
+            <div className="type">
+              <IconTag>
+                {PlantType[plant.type]}
+              </IconTag>
+            </div>
+            <div className="genus">
+              <IconTag>
+                {plant.genus}
+              </IconTag>
+            </div>
+            <div className="family">
+              <IconTag>
+                {plant.family}
+              </IconTag>
+            </div>
+          </div>
           <div className="plant-description">
-            <PlantDescription description={plant.description + plant.description + plant.description}/>
+            <PlantDescription description={plant.description}/>
           </div>
-          <div className="instance-list">
-            <div className="title">植株</div>
-            {plantStore.plantInstances.filter(item => item.plantId === plant._id).map(plantInstance => <div
-              className="plant-instance" key={plantInstance._id}>
-              <div className="instance-index">
-                {plantInstance.instanceIndex}
-              </div>
-              <div className="goto-map">
-                <i className="iconfont icon-fold enter"/>
-              </div>
-            </div>)}
-          </div>
+          {
+            !props.instance && <div className="instance-list">
+              <div className="title">植株</div>
+              {plantStore.plantInstances.filter(item => item.plantId === plant._id).map(plantInstance => <div
+                className="plant-instance" key={plantInstance._id}>
+                <div className="instance-index">
+                  {plantInstance.instanceIndex}
+                </div>
+                <div className="position">
+                  二教
+                </div>
+                <div className="like">
+                  <div className="icon">
+                    <i className="iconfont icon-thumb"/>
+                  </div>
+                  <div className="count">{plantInstance.likes || 0}</div>
+                </div>
+                <div className="goto-map" onClick={() => gotoMap(plantInstance)}>
+                  <i className="iconfont icon-fold enter"/>
+                </div>
+              </div>)}
+            </div>
+          }
           <div className="">
 
           </div>
--- src/views/vrView/index.tsx
+++ src/views/vrView/index.tsx
@@ -1,11 +1,46 @@
 // @flow
 import * as React from 'react';
+import {IconButton} from "../../component/iconButton";
+import './index.scss'
 
 type Props = {};
+const htmlAFRAME = `<a-scene>
+  <a-assets>
+    <audio id="click-sound" src="https://cdn.aframe.io/360-image-gallery-boilerplate/audio/click.ogg"></audio>
+
+    <!-- Images. -->
+    <img id="city" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/city.jpg">
+    <img id="city-thumb" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-city.jpg">
+    <img id="cubes" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/cubes.jpg">
+    <img id="cubes-thumb" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-cubes.jpg">
+    <img id="sechelt" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/sechelt.jpg">
+    <img id="sechelt-thumb" src="https://cdn.aframe.io/360-image-gallery-boilerplate/img/thumb-sechelt.jpg">
+  </a-assets>
+
+  <!-- 360-degree image. -->
+  <a-sky id="image-360" radius="10" src="#city"></a-sky>
+
+  <!-- Link template we will build. -->
+  <a-entity class="link"></a-entity>
+
+  <!-- Camera + Cursor. -->
+  <a-camera>
+    <a-cursor
+      id="cursor"
+      animation__click="property: scale; from: 0.1 0.1 0.1; to: 1 1 1; easing: easeInCubic; dur: 150; startEvents: click"
+      animation__clickreset="property: scale; to: 0.1 0.1 0.1; dur: 1; startEvents: animationcomplete__click"
+      animation__fusing="property: scale; from: 1 1 1; to: 0.1 0.1 0.1; easing: easeInCubic; dur: 150; startEvents: fusing"></a-cursor>
+  </a-camera>
+</a-scene>`
 export const VRView = (props: Props) => {
   return (
     <div className={'vr-view'}>
-      VR
+      <IconButton onClick={() => history.back()} className={'back-button'}>
+        <i className="iconfont icon-back"/>
+      </IconButton>}
+      <div className="vr-container" dangerouslySetInnerHTML={{__html: htmlAFRAME}}>
+
+      </div>
     </div>
   );
 };
